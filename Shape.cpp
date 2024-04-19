111e3ef77cd56787f668203c9b508fcb56c135b6
Vitalya <vitalyabes.vb@gmail.com>
2024-04-19 11:55:02 +0300

    Initial comit

--- Shape.cpp
+++ Shape.cpp
@@ -55,21 +55,6 @@ Shape::Shape(int _type, angle1 ang1, angle2 ang2, angle3 ang3, angle4 ang4, angl
 		break;
 	}
 
-	//// считаем объем фигуры
-	//switch (type)
-	//{
-	//case line:
-	//	volume = 0;
-	//	break;
-	//case sqr:
-	//	volume = 0;
-	//	break;
-	//case cube:
-	//	volume = a * b * c;
-	//	break;
-	//default:
-	//	break;
-	//}
 
 }
 
@@ -105,21 +90,5 @@ Shape::Shape(int type, angle1 ang1, double R, double H)
 	default:
 		break;
 	}
-
-	//// считаем объем фигуры
-	//switch (type)
-	//{
-	//case circle:
-	//	volume = 0;
-	//	break;
-	//case cylinder:
-	//	volume = M_PI * R * R * height;
-	//	break;
-	//default:
-	//	break;
-	//}
-
 }
 
-
-//int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y3, int _z3, int _x4, int _y4, int _z4, int _x5, int _y5, int _z5, int _x6, int _y6, int _z6, int _x7, int _y7, int _z7, int _x8, int _y8, int _z8
